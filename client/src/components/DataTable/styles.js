0442ab95e5a57340dd44dbeace4c99b4f0a5c097
kirsenkodanit <a.kirs@ukr.net>
2020-04-11 15:04:33 +0300

    datatable

--- client/src/components/DataTable/styles.js
+++ client/src/components/DataTable/styles.js
@@ -1,12 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles(theme => ({
-  fab: {
-    margin: theme.spacing(2),
-  },
-  fab: {
-    position: 'fixed',
-    bottom: theme.spacing(5),
-    right: theme.spacing(3),
-  },
+export const useStyles = makeStyles((theme) => ({
+	fab: {
+		margin: theme.spacing(2),
+		position: 'fixed',
+		bottom: theme.spacing(5),
+		right: theme.spacing(3),
+	},
 }));
