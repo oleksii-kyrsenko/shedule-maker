776e3ffccf84bab615707baf027816324b4e0a88
kirsenkodanit <a.kirs@ukr.net>
2020-04-13 18:16:33 +0300

    prepared for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const config = require('config');
 
 const connectDB = require('./db');
 
@@ -28,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || config.get('PORT');
 
 app.listen(PORT, (error) =>
 	error ? console.log(error) : console.log(`Server started on port ${PORT}`)
